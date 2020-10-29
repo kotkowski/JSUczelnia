482f15e72cd3ec877ef2c9996feffc6afdb225dd
kotkowski <kotkowski666@gmail.com>
2020-10-29 09:18:07 +0100

    Commit2

--- ad2/script.js
+++ ad2/script.js
@@ -1,9 +1,26 @@
 document.body.addEventListener('keydown', onKeyPress);
 document.querySelector('#recordBtn').addEventListener('click', onRecordBtn);
 document.querySelector('#playBtn').addEventListener('click', onPlayBtn);
+document.querySelector('#recordBtn2').addEventListener('click', onRecordBtn2);
+document.querySelector('#playBtn2').addEventListener('click', onPlayBtn2);
+document.querySelector('#recordBtn3').addEventListener('click', onRecordBtn3);
+document.querySelector('#playBtn3').addEventListener('click', onPlayBtn3);
+document.querySelector('#recordBtn4').addEventListener('click', onRecordBtn4);
+document.querySelector('#playBtn4').addEventListener('click', onPlayBtn4);
 
 let recordStartTime;
+let recordStartTime2;
+let recordStartTime3;
+let recordStartTime4;
 let recordedSound = [];
+let recordedSound2 = [];
+let recordedSound3 = [];
+let recordedSound4 = [];
+let recording1 = false;
+let recording2 = false;
+let recording3 = false;
+let recording4 = false;
+
 let code = 0;
 function onKeyPress(ev){
     let soundId;
@@ -18,6 +35,7 @@ function onKeyPress(ev){
         else {
             soundId = 'boom';
         }
+        
         code = 0;
         break;
     case 'KeyS':
@@ -96,24 +114,52 @@ function onKeyPress(ev){
         code = 0;
         break;
     }
+
     if(soundId){
-        const soundTime = Date.now() - recordStartTime;
+        let soundTime = Date.now() - recordStartTime;
         const sound = document.querySelector('#'+ soundId);
-        const soundObj = {
+        let soundObj = {
             soundId: soundId,
             time: soundTime
         };
+        const imageId = document.querySelector('#' + soundId + 'Img');
+        imageId.classList.toggle('animated');
+        
         sound.play();
-        recordedSound.push(soundObj);
+        if(recording1){
+            recordedSound.push(soundObj);
+        }
+        if(recording2){
+            soundTime = Date.now() - recordStartTime2;
+            soundObj.soundTime = soundTime;
+            recordedSound2.push(soundObj);
+        }
+        if(recording3){
+            soundTime = Date.now() - recordStartTime3;
+            soundObj.soundTime = soundTime;
+            recordedSound3.push(soundObj);
+        }
+        if(recording4){
+            soundTime = Date.now() - recordStartTime4;
+            soundObj.soundTime = soundTime;
+            recordedSound4.push(soundObj);
+        }
+        
+        
+        
+        imageId.classList.toggle('animated');
     }
     
 }
 
 function onRecordBtn(){
     recordStartTime = Date.now();
+    recording1 = true;
+    recordedSound = [];
 }
 
 function onPlayBtn(){
+    recording1 = false;
     for(let index=0; index < recordedSound.length; index++){
         const soundObj = recordedSound[index];
         
@@ -127,7 +173,79 @@ function onPlayBtn(){
     }
 }
 
+function onRecordBtn2(){
+    recordStartTime2 = Date.now();
+    recording2 = true;
+    recordedSound2 = [];
+}
+
+function onPlayBtn2(){
+    recording2 = false;
+    for(let index=0; index < recordedSound2.length; index++){
+        const soundObj = recordedSound2[index];
+        
+        
+        setTimeout(()=> {
+            playSound(soundObj.soundId);
+        }, soundObj.time
+            
+        );
+        
+    }
+}
+
+function onRecordBtn3(){
+    recordStartTime2 = Date.now();
+    recording3 = true;
+    recordedSound3 = [];
+}
+
+function onPlayBtn3(){
+    recording3 = false;
+    for(let index=0; index < recordedSound3.length; index++){
+        const soundObj = recordedSound3[index];
+        
+        
+        setTimeout(()=> {
+            playSound(soundObj.soundId);
+        }, soundObj.time
+            
+        );
+        
+    }
+}
+
+
+function onRecordBtn4(){
+    recordStartTime2 = Date.now();
+    recording4 = true;
+    recordedSound4 = [];
+}
+
+function onPlayBtn4(){
+    recording4 = false;
+    for(let index=0; index < recordedSound4.length; index++){
+        const soundObj = recordedSound4[index];
+        
+        
+        setTimeout(()=> {
+            playSound(soundObj.soundId);
+        }, soundObj.time
+            
+        );
+        
+    }
+}
+
+
 function playSound(soundId){
     const sound = document.querySelector('#' + soundId);
+    const imageId = document.querySelector('#' + soundId + 'Img');
+    imageId.classList.add('animated');
+    
+    setTimeout(()=> {
+        imageId.classList.remove('animated');
+    }, 1000),
+    
     sound.play();
 }
